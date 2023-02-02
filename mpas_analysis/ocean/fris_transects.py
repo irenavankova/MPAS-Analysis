fab61d7442bb3ec46be9899e542ca31f9ef2c6cc
Irena Vankova <irena.vanek@gmail.com>
2023-02-01 17:11:08 -0700

    activate fris stuff in fris_transects.py

--- mpas_analysis/ocean/fris_transects.py
+++ mpas_analysis/ocean/fris_transects.py
@@ -77,19 +77,19 @@ class FrisTransects(AnalysisTask):
         horizontalBounds = config.getexpression(
             sectionName, 'horizontalBounds')
 
-        observationsDirectory = build_obs_path(
-            config, 'ocean', 'woceSubdirectory')
-
         #observationsDirectory = build_obs_path(
-        #    config, 'ocean', 'frisTransectsSubdirectory')
+        #    config, 'ocean', 'woceSubdirectory')
 
-        origObsFileNames = \
-            {'WOCE_A21': 'WOCE_A21_Drake_Passage_20181126.nc',
-             'WOCE_A23': 'WOCE_A23_South_Atlantic_20181126.nc',
-             'WOCE_A12': 'WOCE_A12_Prime_Meridian_20181126.nc'}
+        observationsDirectory = build_obs_path(
+            config, 'ocean', 'frisTransectsSubdirectory')
 
         #origObsFileNames = \
-        #    {'FRIS_front_2018': 'FRIS_front_transect_2018.nc'}
+        #    {'WOCE_A21': 'WOCE_A21_Drake_Passage_20181126.nc',
+        #     'WOCE_A23': 'WOCE_A23_South_Atlantic_20181126.nc',
+        #     'WOCE_A12': 'WOCE_A12_Prime_Meridian_20181126.nc'}
+
+        origObsFileNames = \
+            {'FRIS_front_2018': 'FRIS_front_transect_2018.nc'}
 
         obsFileNames = {}
         for transectName in horizontalBounds:
